a3130c253371c352111c602271e430805ec1d088
Harshit Jajodia <harshitjajodia@gmail.com>
2021-03-27 02:57:40 +0530

    connected to mongoDb Atlas

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose =require('mongoose');
 
-const db = require('./db')
+//const db = require('./db')
 const pincodeRouter = require('./routes/pincode-router')
 
+mongoose.connect("mongodb+srv://admin:admin@cluster0.1xkaw.mongodb.net/pincodes?retryWrites=true&w=majority", {useUnifiedTopology: true,useNewUrlParser: true});
 const app = express()
-const apiPort = 3000
+const apiPort = 3001
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
